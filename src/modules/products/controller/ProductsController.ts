8709574fdcfb97803b4af7b7693489f85febc693
Aguiar <devaguiar14@gmail.com>
2022-07-24 17:05:17 -0300

    feat: add product controller

---
+++ src/modules/products/controller/ProductsController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import CreateProductService from '../services/CreateProductService';
+import DeleteProductService from '../services/DeleteProductService';
+import ListProductService from '../services/ListProductService';
+import ShowProductService from '../services/ShowProductService';
+import UpdateProductService from '../services/UpdateProductService';
+
+class ProductsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listProducts = new ListProductService();
+
+    const products = await listProducts.handle();
+    return response.json(products);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const showProduct = new ShowProductService();
+
+    const product = await showProduct.handle(id);
+
+    return response.json(product);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+
+    const createProduct = new CreateProductService();
+
+    const product = await createProduct.handle({ name, price, quantity });
+
+    return response.json(product);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { name, price, quantity } = request.body;
+
+    const updateProduct = new UpdateProductService();
+    const product = await updateProduct.handle({ id, name, price, quantity });
+
+    return response.json(product);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteProduct = new DeleteProductService();
+
+    await deleteProduct.handle(id);
+
+    return response.json([]);
+  }
+}
+
+export default ProductsController;
