3a26097bc8647b412949ee6e67fad612c23437c4
Aguiar <devaguiar14@gmail.com>
2022-07-24 16:30:31 -0300

    feat: add product service - ShowProductService

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,19 @@
+import AppError from '@shared/http/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
+import { ProductRepository } from '../typeorm/repositories/ProductRepository';
+
+class ShowProductService {
+  public async handle(id: string): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
+    return product;
+  }
+}
+
+export default ShowProductService;
