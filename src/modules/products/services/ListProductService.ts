d786fc80fe71c96bb9fdcec677695794444f7687
Aguiar <devaguiar14@gmail.com>
2022-07-24 16:28:53 -0300

    remove import AppError

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,4 +1,3 @@
-import AppError from '@shared/http/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductRepository';
