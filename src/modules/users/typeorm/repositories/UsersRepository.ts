4799a15b60326a5bc55f7be024fcbb00b757783b
Aguiar <devaguiar14@gmail.com>
2022-07-24 21:19:51 -0300

    feat: add user repository [findById]

--- src/modules/users/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -12,6 +12,16 @@ class UsersRepository extends Repository<User> {
 
     return user;
   }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
 }
 
 export default UsersRepository;
