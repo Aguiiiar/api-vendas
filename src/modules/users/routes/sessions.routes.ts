9f7d8d6a3018024823a446b3cfa96f254b0fef25
Aguiar <devaguiar14@gmail.com>
2022-07-24 22:22:59 -0300

    feat: add user routes - SessionsRoutes

---
+++ src/modules/users/routes/sessions.routes.ts
@@ -0,0 +1,20 @@
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import SessionController from '../controller/SessionController';
+import UsersController from '../controller/UsersController';
+
+const sessionRoutes = Router();
+const sessionController = new SessionController();
+
+sessionRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  sessionController.create,
+);
+
+export default sessionRoutes;
