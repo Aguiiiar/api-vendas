6d410b12cd9b49d4010797c9db25fc35f4e64c12
Aguiar <devaguiar14@gmail.com>
2022-07-24 23:36:24 -0300

    update app

--- src/shared/http/app.ts
+++ src/shared/http/app.ts
@@ -7,11 +7,13 @@ import routes from './routes';
 import AppError from '@shared/http/errors/AppError';
 import '@shared/typeorm';
 import { errors } from 'celebrate';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use('/api/v1', routes);
 
 app.use(errors());
