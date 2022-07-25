561628d24a4d5dc7e52806dca76b80d393df4d40
Aguiar <devaguiar14@gmail.com>
2022-07-24 22:11:11 -0300

    feat: add user service - CreateSessionService

---
+++ src/modules/users/services/CreateSessionService.ts
@@ -0,0 +1,35 @@
+import AppError from '@shared/http/errors/AppError';
+import { compareSync } from 'bcryptjs';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+// interface IResponse {
+//   user: User;
+// }
+
+class CreateSessionService {
+  public async handle({ email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    const passwordConfirmed = compareSync(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    return user;
+  }
+}
+
+export default CreateSessionService;
