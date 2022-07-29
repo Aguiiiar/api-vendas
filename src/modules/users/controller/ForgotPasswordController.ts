7566cf20b1c14f0a2e3a835a8ea9b3a852270e16
Aguiar <87999627+Aguiiiar@users.noreply.github.com>
2022-07-29 19:40:51 -0300

    feat: add user controller - ForgotPasswordController

---
+++ src/modules/users/controller/ForgotPasswordController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import ListUserService from '../services/ListUserService';
+import SendForgotPasswordService from '../services/SendForgotPasswordService';
+
+class ForgotPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+    const sendForgotPasswordEmail = new SendForgotPasswordService();
+
+    await sendForgotPasswordEmail.handle(email);
+
+    return response.status(204).json();
+  }
+}
+
+export default ForgotPasswordController;
