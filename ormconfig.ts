d47d9cc010bb80561fbe9a3ec886088b468aff28
Aguiar <87999627+Aguiiiar@users.noreply.github.com>
2022-07-29 02:59:44 -0300

    feat: create ormconfig.ts

---
+++ ormconfig.ts
@@ -0,0 +1,13 @@
+export default {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  entities: ['./src/modules/**/typeorm/entities/*.ts'],
+  migrations: ['./src/shared/typeorm/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/shared/typeorm/migrations',
+  },
+};
