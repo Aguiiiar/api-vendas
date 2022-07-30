8c8ce6c17a66a739be7af064be81533e4559e362
Aguiar <87999627+Aguiiiar@users.noreply.github.com>
2022-07-30 14:23:27 -0300

    feat: add user controller - ResetPasswordController

---
+++ src/modules/users/controller/ResetPaswordController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import ResetPasswordService from '../services/ResetPasswordService';
+
+class ForgotPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { password, token } = request.body;
+    const resetPassword = new ResetPasswordService();
+
+    await resetPassword.handle({ password, token });
+
+    return response.status(204).json();
+  }
+}
+
+export default ForgotPasswordController;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,5 @@
 import productsRouter from '@modules/products/routes/products.routes';
-import passwordRoutes from '@modules/users/routes/password';
+import passwordRoutes from '@modules/users/routes/password.routes';
 import sessionRoutes from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
