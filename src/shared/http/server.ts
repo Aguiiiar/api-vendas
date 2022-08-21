0f7e16acb082c28625b2c599cdeea8c2a7a5454a
Aguiiiar <devaguiar14@gmail.com>
2022-08-21 02:50:25 -0300

    Update server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,6 @@
 import logger from '@lib/logger';
 import app from './app';
-const port = process.env.SERVER_PORT;
 
-app.listen(port, () => {
-  logger.info(`Server started on port ${port}! 🏆`);
+app.listen(8080, () => {
+  logger.info(`Server started on port 8080! 🏆`);
 });
