451cf8436eb0355fd92ef39c4ec346cdf50f1e11
Aguiiiar <devaguiar14@gmail.com>
2022-09-09 18:29:01 -0300

    feat: add user service - ShowProfileService

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,23 @@
+import AppError from '@shared/http/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+class ShowProfileService {
+  public async handle({ user_id }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UsersRepository);
+    const user = await userRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
