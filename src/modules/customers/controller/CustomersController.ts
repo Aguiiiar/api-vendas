b33d9cce64c1027fa18f897aff8befea4b367efe
Aguiiiar <devaguiar14@gmail.com>
2022-09-09 22:21:21 -0300

    feat: add customer controller - CustomerController

---
+++ src/modules/customers/controller/CustomersController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import CreateCustomerService from '../services/CreateCustomerService';
+import DeleteCustomerService from '../services/DeleteCustomerService';
+import ListCustomerService from '../services/ListCustomerService';
+import ShowCustomerService from '../services/ShowCustomerService';
+import UpdateCustomerService from '../services/UpdateCustomerService';
+
+class CustomersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listCustomers = new ListCustomerService();
+
+    console.log(request.user.id);
+    const customers = await listCustomers.handle();
+
+    return response.json(customers);
+  }
+
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const showCustomer = new ShowCustomerService();
+
+    const customer = await showCustomer.handle({ id });
+
+    return response.json(customer);
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, email } = request.body;
+    const createCustomers = new CreateCustomerService();
+
+    const customer = await createCustomers.handle({ name, email });
+
+    return response.json(customer);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { name, email } = request.body;
+
+    const updateCustomer = new UpdateCustomerService();
+    const customer = await updateCustomer.handle({ id, name, email });
+
+    return response.json(customer);
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deleteCustomer = new DeleteCustomerService();
+
+    await deleteCustomer.handle({ id });
+
+    return response.json([]);
+  }
+}
+
+export default CustomersController;
