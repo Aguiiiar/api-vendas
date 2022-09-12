a829104a838388a9739e1d251fcd6ea682ed2971
Aguiiiar <devaguiar14@gmail.com>
2022-09-12 15:50:45 -0300

    update: add product repository - findAllByIds

--- src/modules/products/typeorm/repositories/ProductRepository.ts
+++ src/modules/products/typeorm/repositories/ProductRepository.ts
@@ -1,5 +1,9 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, In, Repository } from 'typeorm';
 import Product from '../entities/Product';
+
+interface IFindProducts {
+  id: string;
+}
 
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
@@ -11,5 +15,17 @@ export class ProductRepository extends Repository<Product> {
     });
 
     return product;
+  }
+
+  public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+    const productsId = products.map(product => product.id);
+
+    const existsProduts = await this.find({
+      where: {
+        id: In(productsId),
+      },
+    });
+
+    return existsProduts;
   }
 }
